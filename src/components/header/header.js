10bfb75ca371ce4d713407b0d95d4b8bd368ec18
semivori <semivoriii@gmail.com>
2019-12-29 14:31:08 +0200

    first commit

---
+++ src/components/header/header.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import './header.css';
+
+const Header = () => {
+    return (
+        <div className="header d-flex">
+            <h3>
+                <a href="#1">
+                    Star DB
+                </a>
+            </h3>
+            <ul className="d-flex">
+                <li>
+                    <a href="#2">People</a>
+                </li>
+                <li>
+                    <a href="#3">Planets</a>
+                </li>
+                <li>
+                    <a href="#4">Starships</a>
+                </li>
+            </ul>
+        </div>
+    );
+};
+
+export default Header;
